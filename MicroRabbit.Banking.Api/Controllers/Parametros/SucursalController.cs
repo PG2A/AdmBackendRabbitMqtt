bc5e29e6b1b32e1a2e327c1f8bae8b239aed963a
hlbv17 <helenlbv17@gmail.com>
2023-05-29 17:30:17 -0500

    registros x sucursal

---
+++ MicroRabbit.Banking.Api/Controllers/Parametros/SucursalController.cs
@@ -0,0 +1,26 @@
+﻿using MicroRabbit.Banking.Application.Interfaces.Parametros;
+using MicroRabbit.Banking.Application.Models.Parametros;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers.Parametros
+{
+    [Route("api/sucursal")]
+    [ApiController]
+    public class SucursalController : ControllerBase
+    {
+        private readonly ISucursalServices _sucursalServices;
+
+        public SucursalController(ISucursalServices sucursalServices)
+        {
+            _sucursalServices = sucursalServices;
+        }
+        [EnableCors]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _sucursalServices.Obtener();
+            return Ok(200);
+        }
+    }
+}
--- MicroRabbit.Banking.Application/Models/Parametros/SucursalModel.cs
+++ MicroRabbit.Banking.Application/Models/Parametros/SucursalModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MicroRabbit.Banking.Domain.Models.Parametros
+namespace MicroRabbit.Banking.Application.Models.Parametros
 {
     public class SucursalModel
     {
@@ -12,18 +12,17 @@ namespace MicroRabbit.Banking.Domain.Models.Parametros
         public string Punto_emision { get; set; }
         public string Nombre { get; set; }
         public string Direccion { get; set; }
-        public string? Celular { get; set; }
-        public string? Correo { get; set; }
-        public string? Administrador { get; set; }
-        public bool? Estado { get; set; }
-        public bool? Esmatriz { get; set; }
+        public string Celular { get; set; }
+        public string Correo { get; set; }
+        public string Administrador { get; set; }
+        public bool Estado { get; set; }
+        public bool Esmatriz { get; set; }
         public int ContadorCliente { get; set; }
         public int ContadorProveedor { get; set; }
         public string InicialCliente { get; set; }
         public string Canton { get; set; }
         public string Eslogan { get; set; }
-        public string? NombreComercial { get; set; }
-        public string? UrlApi { get; set; }
-
+        public string NombreComercial { get; set; }
+        public string UrlApi { get; set; }
     }
 }
